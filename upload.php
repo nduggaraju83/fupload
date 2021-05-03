5b903dbee586418949c0cc067f06d4e214327ab1
nduggaraju83 <83490834+nduggaraju83@users.noreply.github.com>
2021-05-04 01:05:59 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 //system('net use \\\\10.0.1.4\\ImageDB');
-$target_dir = "file:\\\\10.0.1.4\\ImageDB/";
+$target_dir = "\\10.0.1.4/ImageDB/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
